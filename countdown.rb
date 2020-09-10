2999f02e692b393e878dc30c4d671e634eb3cd2b
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-10 15:44:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
-
-def countdown
+def countdown(num)
+  countdown_operator = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+    "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+
+def countdown_with_sleep(num)
+  if num >= 5
+    puts num
+    countdown_with_sleep(num-1)
+  end
+end
+
+countdown_with_sleep(5)
+
