89b9ea39b531bc6d1381e951001dfd65648f9dad
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-10 16:20:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,10 +13,11 @@ countdown(10)
 
 def countdown_with_sleep(num)
   while num > 0
-    puts num
-    countdown_with_sleep(num-1)
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    num -= 1
   end
+  puts "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(5)
 
